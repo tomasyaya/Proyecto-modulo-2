f6129cdc88f41cb8855f81ea90def4ec41568995
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-06-14 23:54:41 +0200

    guardarElementoMenu

--- routes/elementoMenu.js
+++ routes/elementoMenu.js
@@ -4,6 +4,23 @@ const mongoose = require('mongoose');
 const ElementoMenu = require('../models/modelo-elemento-menu');
 
 
+router.get('/menu/:id/editar', (req, res, next) => {
+  ElementoMenu.find()
+    .then(elementoMenu => { 
+      console.log(elementoMenu);
+      res.render('menu/editMenu', { elementoMenu: elementoMenu }) })
+    .catch(e => console.log(e))
+});
+
+router.post('/menu/:id/editar', (req, res, next) => {
+  const { nombre, precio } = req.body;
+  const idMenu= req.params.id;
+  ElementoMenu.create({nombre:nombre, precio:precio,idMenu:idMenu})
+  .then(menu => {res.redirect('/menu/:id/editar') })
+  .catch(e => console.log(e))
+});
+
+
 
 
 
