caa91182c875bad8458ca56d20afa05cd400dc8e
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-06-18 23:34:00 +0200

    pindone

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -12,6 +12,10 @@ $(document).ready(function(){
     $("#profile-container").removeClass("hidden");
   });
 
+  $(".irMenu").click(event=>{
+    let pin=$(".pinInput").val();
+    window.location.href = `/${pin}`
+  })
   // $.post('/api/restaurante/:id/editar', function(resultado){
 
   // })
