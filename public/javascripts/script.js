0fc5b6b978601a58237ac8bb0edd68603f9e8f82
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-06-21 15:00:07 +0200

    deleteRestuante

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,23 +6,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $(".addRestaurante").click(event=>{
+  $(".addRestaurante").click(event => {
     $("#profile-container").toggleClass("hidden");
   });
 
-  $(".irMenu").click(event=>{
-    let pin=$(".pinInput").val();
+  $(".irMenu").click(event => {
+    let pin = $(".pinInput").val();
     window.location.href = `/${pin}`
   })
 
-  $(".menu-name").click(function(event){
+  $(".menu-name").click(function (event) {
     $(this).parent().find(".menu-container").toggleClass("hidden");
   });
 
+  // $(".button-name-editar").click(function (event) {
+  //   let $this=$(this);
+  //   let $box= $this.parent().parent().siblings(".form-container")
+  //   $box.find(".edit-container").toggleClass("hidden");
+  //   $(this).parent().find(".card-body").addClass("hidden");
+  // });
 
-  // $.post('/api/restaurante/:id/editar', function(resultado){
+  $(".button-name-editar").click(function (event){
+    let popup = $("myPopup");
+    popup.toggleClass("show");
+  }
 
-  // })
+  
+  
+
 })
