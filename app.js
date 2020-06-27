e9fbe62b911926216bb40fac2ce73757de9d36cf
Alberto Correia <alberto_cpv@hotmail.com>
2020-06-27 14:11:13 +0200

    fix

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 hbs.registerPartials(__dirname + 'views/partials');
