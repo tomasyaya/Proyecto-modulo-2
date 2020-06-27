d024085bd987882dfb2c0b0998e8f79490e1d346
Alberto Correia <alberto_cpv@hotmail.com>
2020-06-27 13:00:26 +0200

    fix

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const path         = require('path');
 
 //Conexion bases de datos
 mongoose
-  .connect('mongodb://localhost/proyectomodulo2', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
